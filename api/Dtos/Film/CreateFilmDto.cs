94afb1ac6723673c316073d5e255b28bce2fe310
Sara De Clara <saradeclara@gmail.com>
2024-08-23 18:01:10 +0100

    api: annotations added to create/update dtos

--- api/Dtos/Film/CreateFilmDto.cs
+++ api/Dtos/Film/CreateFilmDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,13 @@ namespace api.Dtos.Film
 {
     public class CreateFilmDto
     {
-        public required string Title { get; set; }
+        public const int MinLen = 5;
+        public const int MaxLen = 100;
+
+
+        [Required]
+        [MinLength(MinLen, ErrorMessage = "Text must be at least {1} characters long.")]
+        [MaxLength(MaxLen, ErrorMessage = "Text can only be {1} characters long.")]
+        public string Title { get; set; } = string.Empty;
     }
 }
